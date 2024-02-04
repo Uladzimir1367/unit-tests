55a5689546b8a93b8f88ff1098d2c8efe353c274
Uladzimir1367 <130497737+Uladzimir1367@users.noreply.github.com>
2024-02-04 22:02:07 +0300

    первое дз

---
+++ unit-tests-1/src/seminars/first/Shop/Product.java
@@ -0,0 +1,28 @@
+package seminars.first.Shop;
+
+public class Product implements Comparable<Product> {
+    private Integer cost;
+    private String productName;
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    //    упорядочиваем объекты, сравнивая их между собой
+    @Override
+    public int compareTo(Product product) {
+        return this.getCost().compareTo(product.getCost());
+    }
+}
